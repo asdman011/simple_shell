8f35fe1bfb75d6133b9ecea96917ade5dda79c4e
Ammar Ashour <am9370337@gmail.com>
2024-01-10 02:46:59 +0200

    update loop.c

--- command_execution_loop.c
+++ command_execution_loop.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * hsh - Main shell loop function.
  *
- * Return: 0 on success, 1 on error, or error code
+ * @info: Parameter & return info struct.
+ * @av: Argument vector from main().
+ *
+ * Return: 0 on success, 1 on error, or error code.
  */
 int hsh(info_t *info, char **av)
 {
@@ -44,13 +45,14 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin_command - finds a builtin command
- * @info: the parameter & return info struct
+ * find_builtin_command - Finds a builtin command in the shell.
+ *
+ * @info: Parameter & return info struct.
  *
  * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ *         0 if builtin executed successfully,
+ *         1 if builtin found but not successful,
+ *        -2 if builtin signals exit().
  */
 int find_builtin_command(info_t *info)
 {
@@ -78,10 +80,11 @@ int find_builtin_command(info_t *info)
 }
 
 /**
- * locate_cmd - finds a command in PATH
- * @info: the parameter & return info struct
+ * locate_cmd - Locates a command in the PATH.
  *
- * Return: void
+ * @info: Parameter & return info struct.
+ *
+ * Return: void.
  */
 void locate_cmd(info_t *info)
 {
@@ -120,10 +123,11 @@ void locate_cmd(info_t *info)
 }
 
 /**
- * execute_command - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
+ * execute_command - Forks an exec thread to run a command.
  *
- * Return: void
+ * @info: Parameter & return info struct.
+ *
+ * Return: void.
  */
 void execute_command(info_t *info)
 {
