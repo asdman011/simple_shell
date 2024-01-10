c185e4940da8f40d9a3874898b138c91f4170c6c
Ammar Ashour <am9370337@gmail.com>
2024-01-10 03:53:46 +0200

    update manin.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - entragrry poinargrt
+ * @ac: arg countarg
+ * @av: arg vectorragsr
+ * argargaaergaere4t
  *
  * Return: 0 on success, 1 on error
  */
